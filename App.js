fbc585559f4334d40c52ff1188d9529a7af3cc72
FATMA YILDIZ AKÇİÇEK <fatmayildiz@msn.com>
2023-11-21 10:50:07 +0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, StyleSheet, Text, SafeAreaView } from 'react-native';
+
+const CustomButton = ({ title, width, height, borderRadius, borderWidth, borderColor }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePress = () => {
+    setIsPressed(true);
+  };
+
+  useEffect(() => {
+    let timeout;
+    if (isPressed) {
+      timeout = setTimeout(() => {
+        setIsPressed(false);
+      }, 2000); // 2000 milisaniye (2 saniye) sonra küçült
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isPressed]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ 
+
+
+   
+    <TouchableOpacity
+      style={[
+        styles.button,
+        isPressed && styles.pressedButton,
+        isPressed && { backgroundColor: '#FFA500' },
+        width && { width },
+        height && { height },
+        borderRadius && { borderRadius },
+        borderWidth && {borderWidth},
+        borderColor && {borderColor},//buradaki stiller kulanıcının tercihine göre değişebilir.
+      ]}
+      onPress={handlePress}
+    >
+      <Text style={[styles.buttonText, isPressed && styles.pressedText]}>
+      Custom Button {title}
+      </Text>
+    </TouchableOpacity>
+
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  button: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    width:140,
+    height:40,
+  },
+  buttonText: {
+    fontSize: 16,
+    color:'blue',
+  },
+  pressedButton: {
+    transform: [{ scale: 1.2 }],
+  },
+  pressedText: {
+    color: 'white',
   },
 });
+
+export default CustomButton;
